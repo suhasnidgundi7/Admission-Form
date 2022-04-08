c13e3b5a5c6af4917c6988449a5aba5927d2e9f6
Suhas Nidgundi <suhasnidgundi@gmail.com>
2022-04-09 01:38:54 +0530

    Allowing host

--- svcp_student/svcp_student/settings.py
+++ svcp_student/svcp_student/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-74ec*j+y3&$9c(lrfzplnq3__d8a#s#g=3gp+gopfx&(*=^ms!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['svcpadmission.herokuapp.com']
 
 
 # Application definition
