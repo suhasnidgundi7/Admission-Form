ab82b6c0d80c1baed2a606fccda28cded11060a9
Suhas Nidgundi <suhasnidgundi@gmail.com>
2022-04-09 02:37:29 +0530

    Favicon Added

--- svcp_student/svcp_student/urls.py
+++ svcp_student/svcp_student/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('admission.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path(r'^favicon\.ico',RedirectView.as_view(url='/static/images/favicon.ico')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Admin Panel'                    
