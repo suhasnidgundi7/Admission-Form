403464402676c6bfbd8ae0ce008e69d54b0373a5
Suhas Nidgundi <suhasnidgundi@gmail.com>
2022-04-09 02:40:39 +0530

    Favicon Removed

--- svcp_student/svcp_student/urls.py
+++ svcp_student/svcp_student/urls.py
@@ -17,13 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('admission.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path(r'^favicon\.ico',RedirectView.as_view(url='/static/images/favicon.ico')),
+    path('api-auth/', include('rest_framework.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Admin Panel'                    
